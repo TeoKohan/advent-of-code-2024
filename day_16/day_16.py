3d19c8d248809f6446c0b922d76286b6a82db601
Teo <teo.kohan@gmail.com>
2024-12-17 01:57:12 -0300

    Remove Vector2 Hash Dependency

--- day_16/day_16.py
+++ day_16/day_16.py
@@ -3,7 +3,7 @@ from math import inf
 from collections import defaultdict
 from vector2 import Vector2
 
-type FrozenV2 = tuple[int, int]
+type FVector2 = tuple[int, int]
 sys.setrecursionlimit(15000)
 
 mov_cost = 1
@@ -17,18 +17,19 @@ with open('input') as input:
     end   = next(Vector2(line.index('E'), i) for i, line in enumerate(maze) if 'E' in line)
 
 
-def to_tuple(reindeer: Vector2, direction: Vector2) -> tuple[FrozenV2, FrozenV2]:
+def to_tuple(reindeer: Vector2, direction: Vector2) -> tuple[FVector2, FVector2]:
     return tuple(reindeer), tuple(direction)
 
 reindeer = Vector2(*start)
-solutions: defaultdict[tuple[FrozenV2, FrozenV2], int] = defaultdict(lambda: inf)
+solutions: defaultdict[tuple[FVector2, FVector2], int] = defaultdict(lambda: inf)
 
-def flood(M: dict[tuple[FrozenV2, FrozenV2], int], maze: list[list[str]], end: Vector2):
+def flood(M: dict[tuple[FVector2, FVector2], int], maze: list[list[str]], end: Vector2):
     M |= {(tuple(end), tuple(direction)): 0 for direction in Vector2.orthogonal()}
-    to_consider: set[tuple[Vector2, Vector2]] = {(end, direction) for direction in Vector2.orthogonal()}
+    to_consider: set[tuple[FVector2, FVector2]] = {to_tuple(end, direction) for direction in Vector2.orthogonal()}
 
     while to_consider:
         position, direction = to_consider.pop()
+        position, direction = Vector2(*position), Vector2(*direction)
         state = to_tuple(position, direction)
         cost = M[state]
         while   (position := position - direction) and\
@@ -36,29 +37,30 @@ def flood(M: dict[tuple[FrozenV2, FrozenV2], int], maze: list[list[str]], end: V
                 maze[position.y][position.x] != '#':
             cost += 1
             if cost < M[to_tuple(position, direction)]:
-                to_consider.add((position, direction))
+                to_consider.add(to_tuple(position, direction))
                 M[to_tuple(position, direction)] = cost
             if cost + rot_cost < M[to_tuple(position, direction.rotated_clockwise())]:
-                to_consider.add((position, direction.rotated_clockwise()))
+                to_consider.add(to_tuple(position, direction.rotated_clockwise()))
                 M[to_tuple(position, direction.rotated_clockwise()        )] = cost + rot_cost
             if cost + rot_cost < M[to_tuple(position, direction.rotated_counter_clockwise())]:
-                to_consider.add((position, direction.rotated_counter_clockwise()))
+                to_consider.add(to_tuple(position, direction.rotated_counter_clockwise()))
                 M[to_tuple(position, direction.rotated_counter_clockwise())] = cost + rot_cost
 
-def best_tiles(M: dict[tuple[FrozenV2, FrozenV2], int], maze: list[list[str]], start: Vector2):
+def best_tiles(M: dict[tuple[FVector2, FVector2], int], maze: list[list[str]], start: Vector2):
     solution: set[tuple[int, int]] = set()
-    to_consider: set[tuple[Vector2, Vector2]] = {(start, Vector2(1, 0))}
+    to_consider: set[tuple[FVector2, FVector2]] = {to_tuple(start, Vector2(1, 0))}
 
     while to_consider:
         position, direction = to_consider.pop()
+        position, direction = Vector2(*position), Vector2(*direction)
         solution.add(tuple(position))
         cost = M[to_tuple(position, direction)]
         if M[to_tuple(position+direction, direction)] == cost - mov_cost:
-            to_consider.add((position+direction, direction))
+            to_consider.add(to_tuple(position+direction, direction))
         if M[to_tuple(position, direction.rotated_clockwise())] == cost - rot_cost:
-            to_consider.add((position, direction.rotated_clockwise()))
+            to_consider.add(to_tuple(position, direction.rotated_clockwise()))
         if M[to_tuple(position, direction.rotated_counter_clockwise())] == cost - rot_cost:
-            to_consider.add((position, direction.rotated_counter_clockwise()))
+            to_consider.add(to_tuple(position, direction.rotated_counter_clockwise()))
     
     return solution
 
