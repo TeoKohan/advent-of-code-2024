fa35aef738a99788b9e3f513fbdb008ab0ac6a23
Teo <teo.kohan@gmail.com>
2024-12-17 01:32:37 -0300

    Minor Refactor

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -3,19 +3,14 @@ from vector2 import Vector2
 with open('input') as input:
     data = input.read().splitlines()
     data = list(map(list, data))
-
-n, m = len(data), len(data[0])
-
-for y, line in enumerate(data):
-    if '^' in line:
-        x = line.index('^')
-        break
-guard = Vector2(x, y)
-data[y][x] = '.'
+    n, m = len(data), len(data[0])
+    guard = next(Vector2(line.index('^'), i) for i, line in enumerate(data) if '^' in line)
+    data[guard.y][guard.x] = '.'
 
 def get_path(data: list[list[str]], guard: Vector2) -> set[tuple[int, int]]:
     direction: Vector2 = Vector2(0, -1)
     visited = set()
+
     while 0 <= guard.x < m and 0 <= guard.y < n:
         visited.add(tuple(guard))
         
@@ -23,11 +18,11 @@ def get_path(data: list[list[str]], guard: Vector2) -> set[tuple[int, int]]:
         if 0 <= x < m and 0 <= y < n and data[y][x] == '#':
             direction.rotate_clockwise()
         else:
-            guard += direction
+            guard = guard + direction
     
     return visited
 
-def loops(data, guard, obstacle: Vector2):
+def loops(data: list[list[str]], guard: Vector2, obstacle: Vector2):
     data[obstacle.y][obstacle.x] = '#'
     
     direction = Vector2(0, -1)
@@ -39,16 +34,16 @@ def loops(data, guard, obstacle: Vector2):
         if 0 <= x < m and 0 <= y < n and data[y][x] == '#':
             direction.rotate_clockwise()
         else:
-            guard += direction
+            guard = guard + direction
 
     data[obstacle.y][obstacle.x] = '.'
     return 0 <= guard.x < m and 0 <= guard.y < n
 
-def count_looping(data: list[list[str]], guard: Vector2, obstacle_positions: set[Vector2]) -> int:
+def count_looping(data: list[list[str]], guard: Vector2, obstacle_positions: set[tuple[int, int]]) -> int:
     return sum([loops(data, guard, Vector2(i, j)) for i, j in obstacle_positions if Vector2(i, j) != guard])
 
-obstacle_positions = get_path(data, guard)
+obstacle_positions = get_path(data, Vector2(*guard))
 
 with open('output', 'w') as output:
     output.write( str(len(obstacle_positions)) + '\n')
-    output.write( str(count_looping(data, guard, obstacle_positions)) + '\n')
+    output.write( str(count_looping(data, Vector2(*guard), obstacle_positions)) + '\n')
